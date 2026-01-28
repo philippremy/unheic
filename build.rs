6bce4647e5c7bd83fc3e9d4f5284bc6d80d63543
Philipp Remy <philipp@philippremy.net>
2026-01-28 17:23:32 +0100

    [PATCH v1] Fix static linkage of native turbojpeg on <ARCH>-pc-windows-<ENV> targets
    Signed-off-by: Philipp Remy <philipp@philippremy.net>

--- build.rs
+++ build.rs
@@ -1,9 +1,17 @@
 fn main() {
     
-    println!("cargo:rustc-link-lib=static=c++");
-    println!("cargo:rustc-link-lib=static=pthread");
-    println!("cargo:rustc-link-arg={}", "-static");
-    println!("cargo:rustc-link-arg={}", "-static-libstdc++");
-    println!("cargo:rustc-link-arg={}", "-static-libgcc");
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+    
+    if target_os == "windows" {
+        println!("cargo:warning={}", "Defaulting to a full static build on Windows!");
+        println!("cargo:rustc-link-lib=static=c++");
+        println!("cargo:rustc-link-lib=static=pthread");
+        println!("cargo:rustc-link-arg={}", "-static");
+        println!("cargo:rustc-link-arg={}", "-static-libstdc++");
+        println!("cargo:rustc-link-arg={}", "-static-libgcc");
+    } else {
+        println!("cargo:rustc-link-lib=c++");
+        println!("cargo:rustc-link-lib=pthread");
+    }
     
 }
